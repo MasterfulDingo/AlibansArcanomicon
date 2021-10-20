255a84fc6988baeec0ae8ba445f860887ad1ca26
MasterfulDingo <Lachlan.Row@gmail.com>
2021-10-20 13:35:38 +1300

    v0.3.0 - second stage of development complete jinja commented html files fixed bug with concentration and ritual filters partially commented python files

--- main.py
+++ main.py
@@ -21,11 +21,12 @@ import models
 def hello():
     return render_template("home.html")
 
+
 @app.route("/spells") #spells page, shows all spells and has links to their individual pages
 def all_spells():
     spells = models.Spell.query.order_by(models.Spell.name).all()
-
     return render_template("all_spells.html", spells = spells)
+
 
 @app.route("/spell/<int:id>") #spell page, shows all information for a single spell (decided by the id in the url)
 def spell(id):
@@ -33,17 +34,20 @@ def spell(id):
     # tags = models.Tag.query.filter(models.Tag.spells.any(id=id)).all() #for use when I have data in my tag table, currently it will just slow things down.
     return render_template("spell.html", spell=spell)
 
+
 @app.route("/casters") #casters page, shows all casters, links to their individual pages and has a brief blurb
 def all_casters():
     casters = models.Caster.query.order_by(models.Caster.name).all()
 
     return render_template("all_casters.html", casters=casters)
 
+
 @app.route("/caster/<id>") #caster page, shows all spells available to a single caster decided by the id in the url
 def caster(id):
     caster = models.Caster.query.filter_by(id=id).first_or_404()
     spells = models.Spell.query.filter(models.Spell.casters.any(id=id)).all()
     return render_template("caster.html", caster=caster, spells=spells)
+
 
 @app.route("/search", methods=("GET","POST"))
 def search():
@@ -55,23 +59,24 @@ def search():
 
         if bool(params) == False: #if no parameters are submitted, return full results
             return render_template("search.html", spells=spells, schools=schools, casters=casters)
+
         else:
-            # params = ["level_'4'", 'school_conjuration', 'concentration_1', 'ritual_0', 'caster_Bard']
+            #defining variables
             spellslists = []
             spellsets = []
             levelspellslists = []
-            schoolspellslists= []
+            schoolspellslists = []
+            concentrationspellslists = [] #both concentration and ritual are binary values, every spell either has them or doesn't. this means that if both boxes are ticked, either no spells will be returned (filter by intersection, 1st iteration), or all spells will be returned (probably what the user wants, what i decided to make). this could be avoided by searching the params list and if two values of either concentration or ritual are returned, simply discounting those parameters, as they do not filter anything in this case. While this would be more efficient, I could find no efficient or pretty way to do so, and so have decided to simply take the spells they return and process them like everything else.
+            ritualspellslists = []
             intersection = True #if user wants intersection, not union
 
-            for param in params:
+            for param in params: #loops through each parameter returned and split it into the category being filtered and the filter itself
                 param = param.split("_")
 
-
-                if param[0] == "Union":
+                if param[0] == "Union": #this tells the function whether to take only items that meet all of the multiple paramaters, or to take any that meet one or more of multiple paramaters. this provides users more freedom and flexibility in finding spells they need.
                     intersection = False
 
-
-                if param[0] == "level" or param[0] == "school":
+                if param[0] == "level" or param[0] == "school" or param[0] == "concentration" or param[0] == "ritual": #these are discrete variables with no overlap, as you cannot have a spell that is both level 1 and 4 or that is both transmutation and evocation (as defined by the game rules). Because of this, we turn the returned spell lists into sets and combine them so all results are shown.
                     if param[0] == "level":
                         templist = models.Spell.query.filter_by(level=param[1]).all()
                         levelspellslists.append(templist)
@@ -80,61 +85,61 @@ def search():
                         templist = models.Spell.query.filter_by(school=int(param[1])).all()
                         schoolspellslists.append(templist)
 
-
-                if param[0] == "concentration" or param[0] == "ritual" or param[0] == "caster":
                     if param[0] == "concentration":
                         templist = models.Spell.query.filter_by(concentration=int(param[1])).all()
+                        concentrationspellslists.append(templist)
 
                     if param[0] == "ritual":
                         templist = models.Spell.query.filter_by(ritual=int(param[1])).all()
+                        ritualspellslists.append(templist)
 
-                    if param[0] == "caster":
-                        templist = models.Spell.query.filter(models.Spell.casters.any(id=param[1])).all()
-
+                if param[0] == "caster": #each spell can be cast by multiple casters, so I take a list of all the spells available to each caster here, and the user can then decide if they want to filter them by union or intersection.
+                    templist = models.Spell.query.filter(models.Spell.casters.any(id=param[1])).all()
                     spellslists.append(templist)
-            
-
-            spellsets = set_maker(spellslists)
-
+            spellsets = set_maker(spellslists) #this function turns a list of lists into a list of sets
 
             if bool(levelspellslists) != False:
                 levelsets = set_maker(levelspellslists)
                 levelspells = levelsets[0].union(*levelsets)
                 spellsets.append(levelspells)
 
-
             if bool(schoolspellslists) != False:
                 schoolsets = set_maker(schoolspellslists)
                 schoolspells = schoolsets[0].union(*schoolsets)
                 spellsets.append(schoolspells)
 
+            if bool(concentrationspellslists) != False:
+                concentrationsets = set_maker(concentrationspellslists)
+                concentrationspells = concentrationsets[0].union(*concentrationsets)
+                spellsets.append(concentrationspells)
+
+            if bool(ritualspellslists) != False:
+                ritualsets = set_maker(ritualspellslists)
+                ritualspells = ritualsets[0].union(*ritualsets)
+                spellsets.append(ritualspells)
 
             if bool(spellsets) == True:
-
                 if intersection == True:
                     spells = list(spellsets[0].intersection(*spellsets))
-                
                 if intersection == False:
                     spells = list(spellsets[0].union(*spellsets))
-
                 spells.sort(key=sort_key)
-            
                 return render_template("search.html", spells=spells, schools=schools, casters=casters)
-            
 
             if bool(spellsets) == False:
                 return render_template("search.html", spells=spells, schools=schools, casters=casters)
     return render_template("search.html", spells=spells, schools=schools, casters=casters)
 
 
-def set_maker(inputlists):
+def set_maker(inputlists): #this function takes a list of lists and turns it into a list of sets
     outputsets = []
     for inputlist in inputlists:
         outputset = set(inputlist)
         outputsets.append(outputset)
     return outputsets
 
-def sort_key(spell):
+
+def sort_key(spell): #this function creates a key by which a list can be filtered.
     return spell.name
 
 
@@ -143,15 +148,9 @@ def searchinstructions():
     return render_template('searchinstructions.html')
 
 
-
 @app.errorhandler(404) #404 handler, if a url for the page is not found the 404 page will be returned
 def page_not_found(e):
     return render_template('404.html'), 404
-
-
-
-
-
 
 
 if __name__ == "__main__":
